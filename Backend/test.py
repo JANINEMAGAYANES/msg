9730243bb2a24f3a95ac0c71ad1c5962dcee55a8
Daniel Walther <daniel.walther@student.uni-tuebingen.de>
2024-06-22 16:16:22 +0200

    test.py fixed

--- Backend/test.py
+++ Backend/test.py
@@ -1,3 +1,8 @@
 import access_db
+import sqlite3  # import sqlite3 module
+from global_variables import DATABASE_PATH
 
-print(access_db.get_patient_by_id(1))
+# connect to the database
+conn = sqlite3.connect(DATABASE_PATH)
+c = conn.cursor()
+print(access_db.get_patient_by_id(c, 1))
