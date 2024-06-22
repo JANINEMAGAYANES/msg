4807298176d357aa88ca2c48b55da5f5930f487e
Lilo Walter <lilo.walter@gmail.com>
2024-06-22 12:17:51 +0200

    give conn as param

--- Backend/access_db.py
+++ Backend/access_db.py
@@ -2,58 +2,53 @@
 import sqlite3
 from global_variables import DATABASE_PATH
 
-# create connection
-conn = sqlite3.connect(DATABASE_PATH)
-
-# create cursor
-c = conn.cursor()
 
 ## Getter functions ##
 
 #################### Patients ####################
 # get patient by id
-def get_patient_by_id(id):
+def get_patient_by_id(conn,c,  id):
     c.execute("SELECT * FROM Patient WHERE id = ?", (id,))
     return c.fetchone()
 
 # get patient by name & surname
-def get_patient_by_name_surname(name, surname):
+def get_patient_by_name_surname(conn, name, surname):
     c.execute("SELECT * FROM Patient WHERE name = ? AND surname = ?", (name, surname))
     return c.fetchone()
 
 # get list of patients
-def get_patients():
+def get_patients(conn):
     c.execute("SELECT * FROM Patient")
     return c.fetchall()
 
 #################### Prescription ####################
 # get prescription by patient id
-def get_prescription_by_patient_id(id):
+def get_prescription_by_patient_id(conn, c, id):
     c.execute("SELECT id FROM Prescription WHERE pat_id = ?", (id,))
     return c.fetchall()
 
 # get drug by id
-def get_drug_by_id(id):
+def get_drug_by_id(conn, id):
     c.execute("SELECT name FROM Drugs WHERE id = ?", (id,))
     return c.fetchone()
 
 # get drug by prescription id
-def get_drugs_by_prescription_id(id):
+def get_drugs_by_prescription_id(conn, c, id):
     c.execute("SELECT drug_id FROM Drug_in_Prescription WHERE prescription_id = ?", (id,))
     return c.fetchall()
 
 # get list of drugs
-def get_drugs():
+def get_drugs(conn):
     c.execute("SELECT id FROM Drugs")
     return c.fetchall()
 
 # get doctor by id
-def get_doctor_by_id(id):
+def get_doctor_by_id(conn, id):
     c.execute("SELECT * FROM Doctor WHERE id = ?", (id,))
     return c.fetchone()
 
 # get pharmacy by id
-def get_pharmacy_by_id(id):
+def get_pharmacy_by_id(conn, id):
     c.execute("SELECT * FROM Pharmacy WHERE id = ?", (id,))
     return c.fetchone()
 
--- Backend/main.py
+++ Backend/main.py
@@ -1,19 +1,47 @@
 from fastapi import FastAPI
+from Patient import Patient 
 import access_db
 
 app = FastAPI()
 
-example_patient = access_db.get_patient_by_id(1)
-
 @app.get("/")
 def read_root():
-    return example_patient
+    return access_db.get_patient_by_id(1)
 
-@app.get("/prescriptions/{patient_id}")
-def read_prescriptions(patient_id: int):
-    #database stuff
-    return {'103': {'annotations': 'blub blub'}}
+@app.get("/drug_overview/{patient_id}")
+def read_drug_overview(patient_id: int):
+    """
+    Endpoint that needs a patient id as parameter and returns the drugs the patient has been prescribed
+    Args:
+        patient_id (int): the id of the patient
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
+    Returns:
+        list: a list of dictionaries in the following format:
+        [{'name': 'IBUPROFEN', 'drug_id': '1243', 'prescriptions': ['103', '105']}]
+    """
+    patient = Patient(patient_id)
+    print(patient)
+    return patient.get_list_of_drugs() #patient.get_list_of_drugs()
+
+@app.get("/prescription_overview/{patient_id}")
+def read_prescription_overview(patient_id: int):
+    """
+    Endpoint that needs a patient id as parameter and all prescriptions of a patient
+    Args:
+        patient_id (int): the id of the patient
+
+    Returns:
+        list: a list of prescription ids:
+        ['103', '105']
+    """
+    patient = Patient(patient_id)
+    patient.get_list_of_drugs()
+    return patient.get_list_of_drugs()
+
+@app.get("/drug/{drug_id}")
+def read_drug(drug_id):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/prescriptions/{prescription_id}")
+def read_prescription(prescription_id):
     return {"item_id": item_id, "q": q}
