1231ae5c9e924628012f98ae5d05abafc85c3e65
janine <janine@upday.com>
2024-06-22 09:31:32 +0200

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Home from './pages/Home';
+import Scan from './pages/Scan';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            MyApp
+                        </Typography>
+                        <Button color="inherit" component={Link} to="/">Home</Button>
+                        <Button color="inherit" component={Link} to="/scan">Scan</Button>
+                    </Toolbar>
+                </AppBar>
+
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/scan" element={<Scan />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
