d52aa050a5fe62fd35361a85eda0934850160360
Lilo Walter <lilo.walter@gmail.com>
2024-06-22 11:16:26 +0200

    example setup

--- Backend/main.py
+++ Backend/main.py
@@ -6,6 +6,11 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/prescriptions/{patient_id}")
+def read_prescriptions(patient_id: int):
+    #database stuff
+    return {'103': {'annotations': 'blub blub'}}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
